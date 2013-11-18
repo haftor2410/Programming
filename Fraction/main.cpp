78d6a89a6ab09289bd716333555b26565fd6795f
Hafþór Gunnlaugsson <Haftor@Hafors-MacBook-Pro.local>
2013-11-18 13:48:37 +0000

    Initial Commit

---
+++ Fraction/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Fraction
+//
+//  Created by Hafþór Gunnlaugsson on 11/18/13.
+//  Copyright (c) 2013 Hafþór Gunnlaugsson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
