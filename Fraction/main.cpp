fa801abed27388a32f295b9ec6e16702e2ce1deb
Hafþór Gunnlaugsson <Haftor@Hafors-MacBook-Pro-2.local>
2015-01-22 12:52:43 +0000

    l

--- Fraction/main.cpp
+++ Fraction/main.cpp
@@ -1,18 +1,77 @@
-//
-//  main.cpp
-//  Fraction
-//
-//  Created by Hafþór Gunnlaugsson on 11/18/13.
-//  Copyright (c) 2013 Hafþór Gunnlaugsson. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[])
+class Fraction
 {
+private:
+    double dNumenator;
+    double dDenominator;
+    
+public:
+    void input_numerator();
+    void input_denominator();
+    
+    void lesa();
+    void birta();
+    
+    friend bool operator < (Fraction& left, Fraction& right);
+    friend void operator << (ostream& ut, Fraction& right);
+    
+};
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+int main()
+{
+    const int n = 10;
+    
+    Fraction arr[n];
+    
+    // Input ten fractions into the array
+    for (int i = 0; i < n; i++)
+    {
+        arr[i].input_numerator();
+        arr[i].input_denominator();
+    }
+    
+    // Bubble sort the fractions; do not try this at home
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                Fraction tmp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = tmp;
+            }
+        }
+    }
+    
+    // Output the fractions
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i];
+    }
+    
     return 0;
 }
+void Fraction::input_numerator()
+{
+    cin >> dNumenator;
+}
 
+void Fraction::input_denominator()
+{
+    cin >> dDenominator;
+}
+
+bool operator < (Fraction& left, Fraction& right)
+{
+    return (left.dNumenator / left.dDenominator) < (right.dNumenator / right.dDenominator);
+}
+
+void operator << (ostream& ut, Fraction& right)
+{
+    ut << right.dNumenator << "/" << right.dDenominator << endl;
+    
+}
